74ec5a6952236f3098f3a6b8d5362b95c657499b
wangxin <294157902@qq.com>
2023-03-26 22:39:56 +0800

    评论管理模块

--- src/main/java/org/csu/petstoremanage/PetstoreManageApplication.java
+++ src/main/java/org/csu/petstoremanage/PetstoreManageApplication.java
@@ -1,9 +1,11 @@
 package org.csu.petstoremanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.csu.petstoremanage.persistence")
 public class PetstoreManageApplication {
 
     public static void main(String[] args) {
